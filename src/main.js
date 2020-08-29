d0326fdccf94cbb0b89a17942ad630bf8d6075d1
huangdananhai <hss_test@163.com>
2020-08-30 00:06:18 +0800

    整理目录

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-
+import router from './router'// 引入elementui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入ElementUI样式
+import './style/index.less' // 引入初始化样式
+Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册
 createApp(App).use(router).mount('#app')
